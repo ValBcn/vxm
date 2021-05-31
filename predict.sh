2d7052dcaa38ac7abb0c362e05e05cd2be1f460d
valentin <valentin@ws121924.ca.upf.edu>
2021-05-31 15:36:17 +0200

    adding bash scripts

---
+++ predict.sh
@@ -0,0 +1,1 @@
+python scripts/test.py --epochs 200 --input1 /home/valentin/CapsuleNetworkDetSeg/Segmentation/data_bck/preprocessed/imagesTr/103.npy --input2 data_us/preprocessed/imagesTr/12.npy --weights_dir weights/ --res_dir results/ --multi True --reshape 96
---
+++ preprocessing.sh
@@ -0,0 +1,1 @@
+python scripts/preprocess_img_only.py --dset_path ./data_us/ --output_path ./data_us/
